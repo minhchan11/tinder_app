36902f45fcd00d2ce8f70f8ee1792ace47dd43de
Jiwon Kang <jiwonk42@gmail.com>
2017-03-07 11:42:34 -0800

    add index, users_form, and users.cshtml pages

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,4 +1,3 @@
-
 using Nancy;
 using System;
 using System.Collections.Generic;
@@ -9,21 +8,25 @@ namespace TinderApp
   {
     public HomeModule()
     {
-    //   Get["/"] = _ => {
-    //     return View["index.cshtml"];
-    //   };
-    //
-    //
-    // }
-    // public static Dictionary<string, object> ModelMaker()
-    // {
-    //   Dictionary<string, object> model = new Dictionary<string, object>
-    //   {
-    //     {"Bands", Band.GetAll()},
-    //     {"Venues", Venue.GetAll()},
-    //   };
-    //   return model;
-    // }
+       Get["/"] = _ => {
+         return View["index.cshtml"];
+       };
+       Get["/users"] = _ => {
+           List<User> AllUsers = User.GetAll();
+           return View["users.cshtml", AllUsers];
+       };
+       Get["/users/new"] = _ => {
+         return View["users_form.cshtml"];
+       };
+       Post["/users/new"] = _ => {
+         User user1 = new User(Request.Form["user-name"], Request.Form["user-description"]);
+         user1.Save();
+         return View["users.cshtml", user1];
+       };
+
+       Get[""] = _ => {
+
+       };
     }
   }
 }
