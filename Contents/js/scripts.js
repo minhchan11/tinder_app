f991f0247f02856e628529dbe0bbb3ac3ba059cc
Renee <meiqianye@gmail.com>
2017-03-08 11:20:04 -0800

    add google map

--- Contents/js/scripts.js
+++ Contents/js/scripts.js
@@ -1,6 +1,21 @@
 $(document).ready(function(){
-  $("form").submit(function(event) {
-    var newString = "C:\\Users\\epicodus\\desktop\\tinder_app\\Contents\\Profile\\input\\" + $("input#myFile").val().replace("C:\\fakepath\\","");
-    $("#path").val(newString);
+    $("#encode").click(function(){
+      var geocoder = new google.maps.Geocoder();
+      var address = jQuery('#address').val();
+
+      geocoder.geocode( { 'address': address}, function(results, status) {
+
+        if (status == google.maps.GeocoderStatus.OK) {
+          var latitude = results[0].geometry.location.lat();
+          var longitude = results[0].geometry.location.lng();
+          jQuery('#coordinates').val(latitude+', '+longitude);
+        }
+      })
+    });
+
+    $("form").submit(function(event) {
+      var newString = "C:\\Users\\epicodus\\desktop\\tinder_app\\Contents\\Profile\\input\\" + $("input#myFile").val().replace("C:\\fakepath\\","");
+      $("#path").val(newString);
+    });
+
   });
-});
