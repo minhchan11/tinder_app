0ae4e8b8b1c8da2af40a251fa57b4927291071eb
Jiwon Kang and Renee Mei <jiwon+renee@>
2017-03-09 11:49:16 -0800

    get the avatar up there

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -17,10 +17,10 @@ namespace TinderApp
       Post["/users/new/added"] = _ => {
         User newUser = new User(Request.Form["user-name"], Request.Form["user-description"]);
         newUser.Save();
-        // Avatar testAvatar = new Avatar (Request.Form["value"]);
-        // testAvatar.Save();
-        // testAvatar.Display();
-        // testAvatar.DeleteJpg();
+        Avatar newAvatar = new Avatar (Request.Form["value"]);
+        newAvatar.Save();
+        newAvatar.Display();
+        newAvatar.DeleteJpg();
         // Location newLocation = new Location(Request.Form["location"]);
         // newLocation.Save();
         // newLocation.AddUserToLocation(newUser.userId);
@@ -29,8 +29,8 @@ namespace TinderApp
         // newUser.AddWork(Request.Form["user-work"]);
         // newUser.AddFood(Request.Form["user-food"]);
         // newUser.AddHobby(Request.Form["user-hobby"]);
-        // Dictionary<string, object> Model = new Dictionary<string, object>{{"user", newUser.userId},{"name", newUser.name},{"description", newUser.description}, {"gender", Request.Form["user-gender"]},{"work", Request.Form["user-work"]}, {"food", Request.Form["user-food"]}, {"hobby", Request.Form["user-hobby"]}, {"avatar", testAvatar}};
-        return View["index-loggedin.cshtml", newUser];
+        Dictionary<string, object> Model = new Dictionary<string, object>{{"user", newUser},{"avatar", newAvatar}};
+        return View["index-loggedin.cshtml", Model];
       };
 
       Get["/loggedin/{id}"] = parameters => {
