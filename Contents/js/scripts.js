4a9efc2b8fa2ca6246732f2e4b8cf2ffc74929dd
Jiwon Kang and Renee Mei <jiwon+renee@>
2017-03-09 12:16:24 -0800

    add map to the sign up

--- Contents/js/scripts.js
+++ Contents/js/scripts.js
@@ -14,9 +14,19 @@ $(document).ready(function(){
     });
 
     $("form").submit(function(event) {
-      alert("working!")
       var newString = "C:\\Users\\epicodus\\desktop\\tinder_app\\Contents\\Profile\\input\\" + $("input#myFile").val().replace("C:\\fakepath\\","");
       $("#path").val(newString);
+
     });
+
+    $("#sign-up").click(function(){
+      $(".map").append("<p>" + "Use Current Location" + "</p>" +
+          "<input type='hidden' name='location' id='location' value=''>" +
+          '<div id="map-block">'+
+            '<div id="map"></div>'+
+            '<script type="text/javascript" src="/Contents/js/map.js"></script>'+
+            '<script async defer src="http://maps.google.com/maps/api/js?key=AIzaSyB2yaBqgDWOKFDfzPfifIqwXk_j2OZJnCI&callback=initMap"></script>'+
+          '</div>');
+    })
 
   });
